e342b9461a5a879fe9848456813343603730a31c
Ayush Sharma <ayush.sharma@innovaccer.com>
2017-11-08 22:02:32 +0530

    Initial Commit

---
+++ src/js/actions.js
@@ -0,0 +1,16 @@
+/**
+ * Created by pgotthardt on 14/01/16.
+ */
+export function toggleTodoState(id) {
+    return {
+        type: 'TODO_TOGGLE_DONE',
+        id
+    };
+}
+
+export function addTodo(text) {
+    return {
+        type: 'ADD_TODO',
+        text
+    }
+}
