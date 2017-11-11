fccca15531411c17439071b6e4b4981e9b47903c
Ayush Sharma <ayush.aceit@gmail.com>
2017-11-11 21:35:51 +0530

    Doc blocks update

--- src/js/events.js
+++ src/js/events.js
@@ -8,6 +8,9 @@ import {
 } from './actions';
 
 export function registerEventHandlers() {
+    /**
+     * Add todo listenser
+     */
     listen('click', '#addTodo', event => {
         const todoInput = document.getElementById('todoInput');
         if (todoInput.value){
@@ -18,15 +21,24 @@ export function registerEventHandlers() {
         event.stopPropagation();
     });
 
+    /**
+     * Toggle Todo listener
+     */
     listen('click', '.js_toggle_todo', event => {
         const id = Number.parseInt(event.target.getAttribute('data-id'), 10);
         todos.dispatch(toggleTodoState(id));
     });
-
+    
+    /**
+     * set filter listener
+     */
     listen('change', '.js_toggle_filter', event => {
         todos.dispatch(setFilter(event.target.value));
     });
-
+    
+    /**
+     * Todo remove listener
+     */
     listen('click', '.todo__remove', event => {
       const id = Number.parseInt(event.target.getAttribute('data-id'), 10);
       todos.dispatch(removeTodo(id));
